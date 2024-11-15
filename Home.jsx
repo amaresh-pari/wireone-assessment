03c7a5b412837c4edf5d9d7b82edc0e850441c67
Amaresh <amaresh@Amareshs-MacBook-Air.local>
2024-11-15 16:13:48 +0530

    feat: get location permission and plot on map

---
+++ Home.jsx
@@ -0,0 +1,88 @@
+import MapView, { Marker } from "react-native-maps";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Button,
+} from "react-native";
+import { useState, useEffect, useRef } from "react";
+import * as Location from 'expo-location';
+
+const Home = () => {
+  const [location, setLocation] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchLocation();
+  }, []);
+
+  const fetchLocation = async () => {
+    try {
+      setLoading(true);
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let locationData = await Location.getCurrentPositionAsync({});
+      setLocation(locationData);
+    } catch (error) {
+      console.log('Error fetching location');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if(loading || !location || Object.keys(location).length === 0) {
+    return (<></>);
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+          title="Marker"
+        >
+          <Image
+            source={require("./assets/navigation.png")}
+            style={{ width: 40, height: 40 }}
+            resizeMode="contain"
+          />
+        </Marker>
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  marker: {
+    height: 40,
+    width: 40,
+  }
+});
+
+export default Home;
