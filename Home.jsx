17a44db922f2dbfb5ddf559d34cf498f692f23e6
Amaresh <amaresh@Amareshs-MacBook-Air.local>
2024-11-15 18:23:15 +0530

    feat: search bar and FAB

--- Home.jsx
+++ Home.jsx
@@ -1,20 +1,25 @@
 import MapView, { Marker } from "react-native-maps";
 import {
   StyleSheet,
-  Text,
   View,
   Image,
   ScrollView,
-  Button,
+  SafeAreaView,
+  ActivityIndicator,
+  TouchableOpacity,
 } from "react-native";
 import { useState, useEffect, useRef } from "react";
 import * as Location from "expo-location";
 import chagingPods from "./chargingPodLocations.json";
 import ChargingPodCard from "./ChargingPodCard";
+import { SearchBar } from "@rneui/themed";
 
 const Home = () => {
   const [location, setLocation] = useState();
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [snapshot, setSnapshot] = useState(null);
+  const mapRef = useRef();
 
   useEffect(() => {
     fetchLocation();
@@ -38,13 +43,33 @@ const Home = () => {
     }
   };
 
+  const handleSearchChange = (e) => {
+    setSearch(e);
+  };
+
+  const handleCaptureScreen = async () => {
+    try {
+      const _snapshot = await mapRef.current.takeSnapshot({
+        format: "png",
+        quality: 0.8,
+        result: "file",
+      });
+      setSnapshot(_snapshot);
+    } catch (error) {
+      console.log("Error taking snapshot:", error);
+    }
+  };
+
+  console.log(snapshot);
+
   if (loading || !location || Object.keys(location).length === 0) {
-    return <></>;
+    return <ActivityIndicator />;
   }
 
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={styles.map}
         initialRegion={{
           latitude: location.coords.latitude,
@@ -78,6 +103,24 @@ const Home = () => {
         ))}
       </MapView>
 
+      <SafeAreaView style={styles.searchWrap}>
+        <TouchableOpacity onPress={handleCaptureScreen}>
+          <Image
+            source={require("./assets/camera.png")}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+        <SearchBar
+          placeholder="Search for compatible chargers"
+          lightTheme={false}
+          onChangeText={handleSearchChange}
+          value={search}
+          containerStyle={{ width: "80%", padding: 0, borderRadius: 10 }}
+          inputContainerStyle={{ height: 40, borderRadius: 10 }}
+        />
+      </SafeAreaView>
+
       <ScrollView style={styles.carousel} horizontal>
         {chagingPods.chargers.map((charger, index) => (
           <ChargingPodCard key={index} chargingPod={charger} />
@@ -105,6 +148,19 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     paddingBottom: 20,
+  },
+  searchWrap: {
+    position: "absolute",
+    top: 50,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    height: 40,
+    width: 40,
+    marginRight: 10,
   },
 });
 
